3c8cc310a72835204fc83ff660e185370526e530
Jovita <jovitakattsm2@gmail.com>
2021-03-13 15:06:52 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -9,8 +9,10 @@ using System.Threading.Tasks;
 
 namespace MVCAmarinAssignment
 {
+
     public class Program
     {
+        //
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
